c319e77a02e81548e489fcc6fdf77685998618b6
Pere1J <pere1josep@gmail.com>
2023-01-10 16:05:37 +0100

    added edit function

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
 let data = ["books", "pen"];
+let submit = document.getElementById("submit");
+let input = document.getElementById("input");
+let isEditMode = false;
+let editedItemIndex = null;
+
+let sendToInput = (indexOfData) => {
+  submit.innerText = "Update";
+  input.value = data[indexOfData];
+};
+
+function updateInput() {
+  let index = 0;
+
+  for (const item of data) {
+    if (editedItemIndex == index) {
+      data[index] = input.value;
+      isEditMode = false;
+      submit.innerText = "Add";
+      editedItemIndex = null;
+      input.value = "";
+    }
+    index++;
+  }
+  console.log("confirmar");
+}
 
 function addItem() {
-  data.push(input.value);
+  if (isEditMode) {
+    updateInput();
+  } else {
+    data.push(input.value);
+    input.value = null;
+  }
   render();
 }
 
@@ -9,11 +39,13 @@ console.log(data);
 
 let render = () => {
   let template = "";
+  let i = 0;
   for (const item of data) {
-    template += `<li>${item} <button onclick="removeNameFromTheList(this)" style ='background-color:red'>Delete</button> <button>Edit</button></li>`;
+    template += `<li id=${i}><div>${item}</div> <button onclick="removeNameFromTheList(${i})" style ='color:red'>Delete</button>
+     <button onclick="editThisItem(${i})">Edit</button></li>`;
     console.log(template);
+    i++;
   }
-  let input = document.getElementById("input");
 
   let listDom = document.getElementById("listDom");
   console.log(listDom);
@@ -21,9 +53,15 @@ let render = () => {
   listDom.innerHTML = template;
 };
 
-//https://www.tutorialspoint.com/how-to-add-and-remove-names-on-button-click-with-javascript
-function removeNameFromTheList(e) {
-  e.parentElement.remove();
+function editThisItem(index) {
+  sendToInput(index);
+  editedItemIndex = index;
+  isEditMode = true;
+}
+
+function removeNameFromTheList(i) {
+  data.splice(i, 1);
+  render();
 }
 
 render();
